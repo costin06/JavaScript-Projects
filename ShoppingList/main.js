b26099870a01266afad43ab2fe6bd7653f2d120e
costin06 <35259218+costin06@users.noreply.github.com>
2018-05-23 20:00:46 +0300

    d

--- ShoppingList/main.js
+++ ShoppingList/main.js
@@ -1,18 +1,29 @@
-var x=document.querySelector("#but");
-x.onclick=function a(){
-    var item=document.getElementById("inp");
-    var items=document.querySelector("ul");
+var butonSelector=document.querySelector("#but");
+var item=document.getElementById("inp");
+var items = document.querySelector("ul");
+butonSelector.onclick=function a(){
+   AddDel();
+   AddEdit();
+
+}
+var del=document.getElementById("del");
+del.addEventListener("click",function(){
+    var parent=this.parentNode;
+    parent.parentNode.removeChild(parent);
+    condole.log("deleted");
+})
+    function AddDel(){
     var addedItem=document.createElement("li");
     addedItem.textContent=item.value;
     items.appendChild(addedItem);
     var button = document.createElement("button");
-button.innerHTML = "delete";
-items.appendChild(button);
-items.setAttribute("id","del");   
-}
-var s=document.querySelector("#del");
-s.onclick=function b(){
-    var myList = document.getElementById('list');
-myList.innerHTML = '';
-}
-    
+    button.innerHTML = "delete";
+    button.setAttribute("id","del");   
+    addedItem.appendChild(button);
+    }
+    function AddEdit(){
+        var button2 = document.createElement("button");
+        button2.innerHTML = "edit";
+        button2.setAttribute("id","ed");   
+        addedItem.appendChild(button2);
+    }
